d9998cb108d9046fea96d762118c1a9975e9ddca
bwcompton <bcompton@umass.edu>
2025-04-23 19:50:14 +0000

    oops

--- R/predict_a_raster.R
+++ R/predict_a_raster.R
@@ -1,13 +1,17 @@
 # predict a raster
 
-library(peakRAM)
-# peakRAM(fit('oth'))
 
-path <- '/work/pi_cschweik_umass_edu/marsh_mapping/data/oth/gis/flights'
-x <- list.files(path)
-                                            
-
-rasters <- rast(file.path(path, x))
-names(rasters) <- sub('^(\\d)', 'X\\1', names(rasters))                         # files with leading digit get X prepended by R  
-                
-predicted <- terra::predict(rasters, zz)
+if(FALSE) {
+   
+   library(peakRAM)
+   # peakRAM(fit('oth'))
+   
+   path <- '/work/pi_cschweik_umass_edu/marsh_mapping/data/oth/gis/flights'
+   x <- list.files(path)
+   
+   
+   rasters <- rast(file.path(path, x))
+   names(rasters) <- sub('^(\\d)', 'X\\1', names(rasters))                         # files with leading digit get X prepended by R  
+   
+   predicted <- terra::predict(rasters, zz)
+}
