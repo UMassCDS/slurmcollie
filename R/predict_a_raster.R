128e0300868309b8bc7641fec5c17efc9be90319
bwcompton <bcompton@umass.edu>
2025-04-23 19:54:12 +0000

    Try for more cores, and what the hell, let's actually save the results

--- R/predict_a_raster.R
+++ R/predict_a_raster.R
@@ -7,11 +7,12 @@ if(FALSE) {
    # peakRAM(fit('oth'))
    
    path <- '/work/pi_cschweik_umass_edu/marsh_mapping/data/oth/gis/flights'
+   rpath <- '/work/pi_cschweik_umass_edu/marsh_mapping/data/oth/gis/predicted'
    x <- list.files(path)
    
    
    rasters <- rast(file.path(path, x))
    names(rasters) <- sub('^(\\d)', 'X\\1', names(rasters))                         # files with leading digit get X prepended by R  
    
-   predicted <- terra::predict(rasters, zz)
+   peakRAM(predicted <- terra::predict(rasters, zz, cores = 20, filename = file.path(rpath, 'predicted.tif')))
 }
