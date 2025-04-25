b4a1d1fb16e1168db318b1f27dc7ca0efd272a81
bwcompton <bcompton@umass.edu>
2025-04-25 22:20:25 +0000

    More boring script changes

--- R/prediction script.R
+++ R/prediction script.R
@@ -6,7 +6,7 @@ if(FALSE) {
    library(terra)
    
    
-   fit('oth')
+   fit('oth', years = 2019:2020, reread = TRUE)
    # fit('oth', vars = pickvars(40))
    # the$fit <- readRDS('/work/pi_cschweik_umass_edu/marsh_mapping/models/fit_2025-Apr-24_16-08')
    
@@ -26,8 +26,8 @@ if(FALSE) {
    
    rasters <- rasters[[names(rasters) %in% names(the$fit$fit$trainingData)[-1]]]       # drop bands we don't want
    
-   # clip <- ext(c(-70.86254419, -70.86135362, 42.77072136, 42.7717978))               # small clip
-   clip <- ext(c(-70.86452506, -70.86040917, 42.76976948, 42.77283781))                # larger clip
+   clip <- ext(c(-70.86254419, -70.86135362, 42.77072136, 42.7717978))               # small clip
+   # clip <- ext(c(-70.86452506, -70.86040917, 42.76976948, 42.77283781))                # larger clip
    rasters <- crop(rasters, clip)
    
    peakRAM(predicted <- terra::predict(rasters, the$fit$fit, cores = 18, filename = file.path(rpath, 'predicted3.tif'), overwrite = TRUE, na.rm = TRUE))
