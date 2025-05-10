94a9a81529b173b70c91541a9208e6ee48c9d26f
bwcompton <bcompton@umass.edu>
2025-05-10 21:42:49 +0000

    More messing around with launching batch jobs...it works, but lots of issues

--- R/batch_test.R
+++ R/batch_test.R
@@ -1,9 +1,10 @@
 #' a test thingy
 #' @export
 
-batch_test <- function(rep, item = 'hello ', file = '/work/pi_cschweik_umass_edu/marsh_mapping/reg/btest') {
+batch_test <- function(reps, item = paste0('hello ', format(Sys.time())), file = '/work/pi_cschweik_umass_edu/marsh_mapping/reg/btest') {
    
    
-   writeLines(item, paste0(file, rep, '.txt'))
+   writeLines(item, paste0(file, reps, '.txt'))
    cat('Stuff written to ', file, '\n', sep = '')
+   Sys.sleep(30)
 }
--- inst/scripts/messing_around_with_launching_batch_jobs.R
+++ inst/scripts/messing_around_with_launching_batch_jobs.R
@@ -28,14 +28,48 @@
 
 library(batchtools)
 
-# reg_dir <- '/work/pi_cschweik_umass_edu/marsh_mapping/reg/'
-reg_dir <- NA
-regist <- batchtools::makeRegistry(file.dir = reg_dir, conf.file = system.file('batchtools.conf.R', package = 'saltmarsh'))                                                 # make registry - works
-jobs <- batchtools::batchMap(fun = batch_test, reps = 6:11, reg = batchtools::makeRegistry(file.dir = reg_dir,
-                                                                                  conf.file = system.file('batchtools.conf.R', package = 'saltmarsh')))           # set up jobs - works
+the$login_id <- 'login3'
 
-batchtools::findNotDone()                                                                                                                                         # list jobs not done yet
+# reg_dir <- '/work/pi_cschweik_umass_edu/marsh_mapping/reg/'                                                                                                     # it won't let me use this; dk why
+reg_dir <- NA                                                                                                                                                     # does work with a temporary registry
+regist <- batchtools::makeRegistry(file.dir = reg_dir, conf.file = system.file('batchtools.conf.R', package = 'saltmarsh'))                                       # make registry - works
+# jobs <- batchtools::batchMap(fun = batch_test, reps = 6:11, reg = batchtools::makeRegistry(file.dir = reg_dir,
+#                                                                                   conf.file = system.file('batchtools.conf.R', package = 'saltmarsh')))           # set up jobs - works
+jobs <- batchtools::batchMap(fun = batch_test, reps = 6:11, reg = regist)           # set up jobs - works
+
+findNotDone()                                                                                                                                                     # list jobs not done yet
 findJobs()                                                                                                                                                        # list 'em all
 findNotStarted()
 
-submitJobs(jobs$job.id, reg = regist)
+# probably want to set resources list here
+submitJobs(jobs$job.id, reg = regist)                                                                                                                             # should launch jobs, but fails
+
+
+
+
+# Okay, this section works!!!!!!!!!    But I can't get Slurm job ids, thus can't call get_job_efficiency. Plus, it's not using my slurm template
+library(batchtools)
+
+the$login_id <- 'login3'
+
+tmp = makeRegistry(file.dir = NA, make.default = FALSE)
+jobs <- batchMap(fun = batch_test, reps = 2:5, reg = tmp)           # set up jobs - works
+
+submitJobs(reg = tmp)
+findJobs(reg = tmp)
+getStatus(reg = tmp)
+getErrorMessages(reg = tmp)
+
+# clearRegistry(tmp)
+# 
+##########################################################
+
+# This one uses a slimmed down Slurm template. I renamed the old one as slurm_big.tmpl. I still don't get job ids. Ethan obviously can. Dig in next week.
+
+library(batchtools)
+reg <- makeRegistry(file.dir = "my_registry", conf.file = system.file('slurm.tmpl', package = 'saltmarsh', mustWork = TRUE))
+jobs <- batchMap(fun = batch_test, reps = 2:5, reg = reg)           # set up jobs - works
+submitJobs(reg = reg)
+status <- getStatus(reg = reg)
+print(status)
+# Look for column 'batch.id'
