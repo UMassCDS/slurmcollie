af744603b6ae57a0320c79415a1173bb49631dfc
bwcompton <bcompton@umass.edu>
2025-05-21 15:11:27 +0000

    Nice clean batchtools.conf.R and slurm.tmpl, with script to do a test run; everything works great!

---
+++ inst/batchtools.conf.R
@@ -0,0 +1,19 @@
+# Set up Slurm template. Pass this file to makeRegistry as conf.file.
+# To run properly on Unity, you must include the following line in the "Override Rstudio image location" field
+#    /modules/admin-resources/ood-dev/unity-r_4.4.0.sif
+# Authors: https://github.com/birdflow-science/BirdFlowPipeline/blob/main/inst/batchtools.conf.R, Georgia Stuart, Bradley Compton
+# 21 May 2025
+
+
+cluster.functions <- makeClusterFunctionsSlurm(
+   template = system.file('slurm.tmpl', package = 'saltmarsh', lib.loc = .libPaths(), mustWork = TRUE))
+
+
+default.resources <- list(                # Set defaults. These may be overridden by passing resources to submitJobs.
+   walltime = '00:10:00',                 #    max time to run (hh:mm:ss)
+   memory = 1,                            #    max memory (GB); minimum should be 1 GB (slurm.tmpl changes this from default MB to GB)
+   measure.memory = FALSE,                #    enable memory measurement for getJobStatus; not needed if using get_job_efficiency
+   ncpus = 1,                             #    number of CPUs
+   chunks.as.arrayjobs = FALSE,           #    group selected jobs sequentially; must set up chunks in submitJobs; see help(submitJobs)
+   partition.cpu = 'cpu-preempt,cpu'      #    which partitions jobs can run on
+)
--- inst/scripts/simple_slurm_test.R
+++ inst/scripts/simple_slurm_test.R
@@ -2,23 +2,19 @@
 # When launching RStudio, put /modules/admin-resources/ood-dev/unity-r_4.4.0.sif in the container override field
 
 
-# This function is defined in https://github.com/UMass-UAS-Salt-Marsh/salt-marsh-mapping.git
-# batch_test <- function(reps, file = '/work/pi_cschweik_umass_edu/batch_test/btest/test_') {
-#    
-#    item <- paste0('hello ', format(Sys.time()), '\nNode: ', Sys.info()[['nodename']], '\n')
-#    writeLines(item, f <- paste0(file, reps, '.txt'))
-#    cat('Stuff written to ', f, '\n', sep = '')
-#    Sys.sleep(30)
-# }
 
 
 library(batchtools)
 library(saltmarsh)
 
-unlink('/work/pi_cschweik_umass_edu/batch_test/wednesday/reg01', recursive = TRUE)   # nuke old batchtools registry
+unlink('/work/pi_cschweik_umass_edu/batch_test/wednesday/reg02', recursive = TRUE)   # nuke old batchtools registry
 
-makeRegistry(file.dir = "/work/pi_cschweik_umass_edu/batch_test/wednesday/reg01", conf.file = '/work/pi_cschweik_umass_edu/batch_test/wednesday/batchtools-gs.conf.R')
-jobs <- batchMap(fun = saltmarsh::batch_test, reps = 4:6)
+
+makeRegistry(file.dir = "/work/pi_cschweik_umass_edu/batch_test/wednesday/reg02", 
+             conf.file = system.file('batchtools.conf.R', package = 'saltmarsh', lib.loc = .libPaths(), mustWork = TRUE))
+
+
+jobs <- batchMap(fun = saltmarsh::batch_test, rep = 7:8)
 submitJobs()
 
 
