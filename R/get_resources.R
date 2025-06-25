3ddda6b193061f33386227a597c57d435e08ee7f
bwcompton <bcompton@umass.edu>
2025-06-25 20:22:35 +0000

    Ugh, reserved word

--- R/get_resources.R
+++ R/get_resources.R
@@ -1,17 +1,17 @@
 #' Combine user-supplied resources with function-specific defaults
 #' 
 #' @param user List of user-specified resources; NULL if none
-#' @param function List of function-specified resources
+#' @param func List of function-specified resources
 #' @returns Combined list of resources, with user resources taking precedence
 #' @export
 
 
-get_resources <- function(user, function) {
+get_resources <- function(user, func) {
    
    
    resources <- function
    if(!is.null(user)) {
-      both <- c(user, function)
+      both <- c(user, func)
       resources <- both[unique(names(both))]
    }
    
