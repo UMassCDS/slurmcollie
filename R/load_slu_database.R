f145a244fd24475ac3bbf0d8ec01d0688f3ff91c
bwcompton <bcompton@umass.edu>
2025-08-12 15:45:35 -0400

    Drop force option from load_slu_database; update info help

--- R/load_slu_database.R
+++ R/load_slu_database.R
@@ -4,16 +4,16 @@
 #' already been loaded or if `force = TRUE`.
 #' 
 #' @param database Name of database (should be `jdb`)
-#' @param force If TRUE, always loads the database, replacing the current one if it exists
 #' @export
 
 
-load_slu_database <- function(database = 'jdb', force = FALSE) {
+load_slu_database <- function(database = 'jdb') {
    
    
    f <- file.path(slu$dbdir, paste0(database, '.RDS'))
-   if(is.null(slu[[database]]) | force)                         # if don't have the database or force = TRUE,
+   if(is.null(slu[[database]]))                                # if don't have the database,
       if(!file.exists(f))
          stop('Database ', f, ' doesn\'t exist. Use new_db(\'',database,'\') to create it.')
-      slu[[database]] <- readRDS(f)
+   
+   slu[[database]] <- readRDS(f)
 }
