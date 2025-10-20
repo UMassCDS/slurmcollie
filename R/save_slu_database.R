fe72b0b438391f6742e71e85de6f045f8f6b4966
bwcompton <bcompton@umass.edu>
2025-10-20 15:50:26 -0400

    Put backups in databases/backups to get them out of the way

--- R/save_slu_database.R
+++ R/save_slu_database.R
@@ -1,7 +1,8 @@
 #' Save the specified slurmcollie database 
 #' 
 #' Saves the database in directory `slu$dbdir`. Previous versions are renamed e.g., `jdb_1.RDS`,
-#' `jdb_2.RDS`, etc. Will need a mechanism to delete databases more than a week old (or something).
+#' `jdb_2.RDS`, etc and moved to `backups/`. Everything in `backups/` is save to purge if you're
+#' hurting for disk space.
 #' 
 #' @param database Name of database (should be `jdb`)
 #' @importFrom tools file_path_sans_ext
@@ -23,7 +24,7 @@ save_slu_database <- function(database) {
       else {                                                                                 #    else, rename it as a backup
          l <- file_path_sans_ext(list.files(slu$dbdir, pattern = database))                  #       database filenames
          n <- max(c(0, as.numeric(sub(paste0(database, '_*'), '', l))), na.rm = TRUE) + 1    #       backup file number
-         file.rename(f, file.path(slu$dbdir, paste0(database, '_', n, '.RDS')))
+         file.rename(f, file.path(slu$dbdir, paste0('backups/', database, '_', n, '.RDS')))
       }
    }
    
